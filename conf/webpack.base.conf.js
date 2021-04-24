3e04473cff2f53ccd81c202a7823088889938876
Margaret <match14@yandex.ru>
2021-04-24 13:01:21 +0300

    conf

--- conf/webpack.base.conf.js
+++ conf/webpack.base.conf.js
@@ -33,7 +33,7 @@ module.exports = {
     // module: `${PATHS.src}/your-module.js`,
   },
   output: {
-    filename: `${PATHS.assets}js/[name].[hash].js`,
+    filename: `${PATHS.assets}js/[name].js`,
     path: PATHS.dist,
   },
   optimization: {
@@ -183,7 +183,7 @@ module.exports = {
     }),
     new VueLoaderPlugin(),
     new MiniCssExtractPlugin({
-      filename: `${PATHS.assets}css/[name].[hash].css`,
+      filename: `${PATHS.assets}css/[name].css`,
     }),
     new SpriteLoaderPlugin({
       plainSprite: true
